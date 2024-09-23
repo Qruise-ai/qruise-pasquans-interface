788b289f2277686273879b1b0acfaa79e682bd17
MARCO ROSSIGNOLO <marco.rossignolo@qruise.com>
2024-09-23 12:49:50 +0200

    Removed commented code

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,23 +8,6 @@
 """
 import pytest
 from qruise_pasquans_interface.provider import PasquansProvider
-
-# import sys
-# import pytest
-
-# def is_debugging():
-#     return 'debugpy' in sys.modules
-    
-    
-# # enable_stop_on_exceptions if the debugger is running during a test
-# if is_debugging():
-#   @pytest.hookimpl(tryfirst=True)
-#   def pytest_exception_interact(call):
-#     raise call.excinfo.value
-    
-#   @pytest.hookimpl(tryfirst=True)
-#   def pytest_internalerror(excinfo):
-#     raise excinfo.value
   
 @pytest.fixture
 def mock_simulator():
