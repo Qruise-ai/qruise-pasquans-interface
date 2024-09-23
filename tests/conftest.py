31ba6c35b896710a0a0e3cc474eb2e656e42806a
MARCO ROSSIGNOLO <marco.rossignolo@qruise.com>
2024-09-23 14:23:26 +0200

    Removed commented information

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,3 @@
-"""
-    Dummy conftest.py for qruise_pasquans_interface.
-
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
 import pytest
 from qruise_pasquans_interface.provider import PasquansProvider
   
